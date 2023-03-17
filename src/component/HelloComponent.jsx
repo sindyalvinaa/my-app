8fd0673003a8cdf53104261b8a6fa8378b244b1d
sindyalvinaa <sindyalvina93@gmail.com>
2023-03-17 09:47:01 +0700

    Commit

---
+++ src/component/HelloComponent.jsx
@@ -0,0 +1,10 @@
+// agar function arrow HelloComponent bisa berjalan direact maka tambahkan react
+import React from "react";
+import './HelloComponent.css'
+
+const HelloComponent = () =>{
+    return <p className="text-p">ini adalah arrow function yang ada difolder</p>
+}
+
+export default HelloComponent;
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import Test from './Test'
+// import App from './App';
+// import Tugas from './Tugas/Tugas';
+import HelloComponent from './component/HelloComponent';
 import reportWebVitals from './reportWebVitals';
+
+// const HelloComponent = () =>{
+//   return HelloComponent
+// }
+
+// class StateFullComponent extends React.Component{
+//   render(){
+//     return<p>StateFullComponent</p>
+//   }
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelloComponent />
   </React.StrictMode>
 );
 
@@ -15,3 +28,31 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// // import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import HelloComponent from './component/HelloComponent';
+
+// // function HelloComponent(){
+// //   return HelloComponent
+// // }
+// // class StatefullComponent extends React.Component{
+// // render(){
+// //   return<p>StatefullComponent</p>
+// // }
+// // }
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <HelloComponent />
+//   </React.StrictMode>
+// );
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+ 
